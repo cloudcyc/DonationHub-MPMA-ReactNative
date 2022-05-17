b2a8dcfa0344992e54e65c37503f1d6a9d85139c
Nicolas Chiang <nicolasccl2000@gmail.com>
2022-05-17 22:10:09 +0800

    API update

--- DonationHub/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ DonationHub/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "donationhubusersapi": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     },
     "api": {
