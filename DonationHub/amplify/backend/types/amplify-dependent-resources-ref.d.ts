6d3d2fbe003f2c92652a6c458cf20b848889a9a7
Nicolas Chiang <nicolasccl2000@gmail.com>
2022-05-17 15:37:58 +0800

    Setting up API

---
+++ DonationHub/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "dynamodbDonationHub": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "donationhubapi": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "donationhubapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
