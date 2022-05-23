a9271cf60a7fddcf32eb2c90c749905071f4b4eb
Chow Cheng <nicolasccl2000@gmail.com>
2022-05-23 15:24:13 +0800

    Backend HomeScreen

--- DonationHub/Navigation/screens/Homescreen.js
+++ DonationHub/Navigation/screens/Homescreen.js
@@ -1,40 +1,60 @@
 import * as React from 'react';
-import { Image, StyleSheet, Text, View, useWindowDimensions, ScrollView, TextInput, Button, TouchableOpacity,Pressable } from 'react-native';
+import { Image, StyleSheet, Text, View, useWindowDimensions, ScrollView, TextInput, Button, TouchableOpacity,Pressable,FlatList } from 'react-native';
+
+
+
 
 
 function HomeScreen ({navigation}) {
+    const [centreList, setcentreList] = React.useState([]);
+    const getCentreAPI = 'https://3yerh8al29.execute-api.ap-southeast-1.amazonaws.com/dev/centres';
+    const getCentreList = () => {
+        fetch(getCentreAPI).then((response) => response.json()).then((json) => { 
+            setcentreList(json);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    React.useEffect(() => {
+        getCentreList();
+        
+    },[]);
     return(
         <ScrollView style={styles.root}>
 
             <View style={styles.containerSearch}>
-                <TextInput placeholder='Search Donate Hub'/>
+                <TextInput placeholder='Search Donate Centre'/>
             </View>
-
-            <TouchableOpacity
-                onPress={() => navigation.navigate('LocationDetails')}
-                style={styles.roundButton}>
-                <Image style={styles.icon} source={{
-                uri:
-                    'https://s1.cdn.autoevolution.com/images/news/google-maps-is-getting-a-feature-that-just-makes-sense-these-days-150219_1.jpg',
-                }}/>
-                <Text style={styles.locationtitle}>Pavilion Kuala Lumpur</Text>
-                <Text style={styles.locationtitle2}>Phone:      03-2118 8833</Text>
-                <Text style={styles.locationtitle2}>Location:  Lot 5.100.00 Level 5, Pavilion Elite</Text>
-
-
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Pavilion')}
-                style={styles.roundButton}>
-                <Image style={styles.icon} source={{
-                uri:
-                    'https://s1.cdn.autoevolution.com/images/news/google-maps-is-getting-a-feature-that-just-makes-sense-these-days-150219_1.jpg',
-                }}/>
-                <Text style={styles.locationtitle}>KLCC Malaysia</Text>
-                <Text style={styles.locationtitle2}>Phone:      03-2382 2828</Text>
-                <Text style={styles.locationtitle2}>Location:  Lot 316-A, Level 3</Text>
-            </TouchableOpacity> 
+            <View>
+                <FlatList 
+                        style={styles.list}
+                        data={centreList}
+                        keyExtractor= {(item) => {
+                            return item.centreID;
+                        }}
+                        ItemSeparatorComponent={() => {
+                            return (
+                                <View style={styles.separator}/>
+                            )
+                        }}
+                        renderItem={({item}) => {
+                            return (
+                                <TouchableOpacity
+                                    onPress={() => navigation.navigate('LocationDetails',item)}
+                                    style={styles.roundButton}>
+                                    <Image style={styles.icon} source={{
+                                        uri:'https://s1.cdn.autoevolution.com/images/news/google-maps-is-getting-a-feature-that-just-makes-sense-these-days-150219_1.jpg',
+                                    }}/>
+                                    <Text style={styles.locationtitle}>{item.centreName}</Text>
+                                    {/* <Text style={styles.locationtitle2}>Phone:      03-2118 8833</Text> */}
+                                    <Text style={styles.locationtitle2}>Location:  {item.centreAddress}</Text>
+                                </TouchableOpacity>
+                            )
+                        }}
+                >
+                </FlatList>
+            </View>
         </ScrollView>
     )
 };
@@ -87,6 +107,14 @@ const styles = StyleSheet.create({
         marginTop:20,
         marginBottom:20,
     },
+    //List Design
+    list: {
+        paddingHorizontal: 17,
+        backgroundColor:"#E6E6E6",
+      },
+    separator: {
+        marginTop: 10,
+    }
 
 });
 
